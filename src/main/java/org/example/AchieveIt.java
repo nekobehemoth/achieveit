0906aefd2bc3f83e444faafd2ee738d8c34ed61d
ushakofuiria <Ilya.Ushakov@lotusretails.com>
2025-01-30 22:39:11 +0700

    init commit

---
+++ src/main/java/org/example/AchieveIt.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchieveIt {
+    public static void main(String[] args) {
+        SpringApplication.run(AchieveIt.class, args);
+    }
+}
---
+++ src/test/java/org/example/AchieveItTests.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class AchieveItTests {
+    @Test
+    public void contextLoads(){
+
+    }
+}
